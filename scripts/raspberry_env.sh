65fe9bf13e20cb8f286cc18461f7230ebf9ff4aa
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-05-13 07:57:43 +0300

    Changed the ip-addresses according to the robot net-configuration

--- scripts/raspberry_env.sh
+++ scripts/raspberry_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #export ROS_IP=155.207.33.186
-export ROS_IP=155.207.33.186
-export ROS_MASTER_URI=http://155.207.33.185:11311
+export ROS_IP=10.0.1.1
+export ROS_MASTER_URI=http://10.0.1.2:11311
 #export ROS_MASTER_URI=http://155.207.33.185
 
 source /home/pandora/rpi_ws/devel/setup.bash --extend
