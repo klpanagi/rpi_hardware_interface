4e0931805c61b578f5ca2b6209a2230d26d9abb2
Ubuntu rpi2 <>
2015-05-16 20:12:59 +0000

    Corrected IP adresses in env.sh script

--- scripts/raspberry_env.sh
+++ scripts/raspberry_env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #export ROS_IP=155.207.33.186
-export ROS_IP=10.0.1.1
-export ROS_MASTER_URI=http://10.0.1.2:11311
+export ROS_IP=10.0.1.2
+export ROS_MASTER_URI=http://10.0.1.1:11311
 #export ROS_MASTER_URI=http://155.207.33.185
 
-source /home/pandora/rpi_ws/devel/setup.bash --extend
+source /home/ubuntu/rpi_ws/devel/setup.bash --extend
 
 exec "$@"
