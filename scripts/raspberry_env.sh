7754c62694deac59e1bab06662d362cbd2d48382
Ubuntu rpi2 <>
2015-05-17 14:55:11 +0000

    Removed useless scripts

--- scripts/raspberry_env.sh
+++ scripts/raspberry_env.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#export ROS_IP=155.207.33.186
-export ROS_IP=10.0.1.2
-export ROS_MASTER_URI=http://10.0.1.1:11311
-#export ROS_MASTER_URI=http://155.207.33.185
+export ROS_IP=155.207.33.186
 
 source /home/ubuntu/rpi_ws/devel/setup.bash --extend
 
--- scripts/rpi_ros_network_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MASTER_IP=$1
-MY_IP=$(ifconfig eth0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
-export ROS_MASTER_URI=http://$MASTER_IP:11311
-export ROS_IP=$MY_IP
-echo "ROS_MASTER_URI = $ROS_MASTER_URI"
-echo "ROS_IP = $ROS_IP"
-
