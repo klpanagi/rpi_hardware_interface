212b6d35b7c72ee8586129d38b2c8fd99df5ffdd
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-05-29 17:32:48 +0000

    Changed directory paths + valid with new structure

--- scripts/raspberry_env.sh
+++ scripts/raspberry_env.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-ETH_IP=$(ifconfig eth1 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+ETH_IP=$(ifconfig eth0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
 echo $ETH_IP
 export ROS_IP=$ETH_IP
 
-source /home/$USER/rpi_ws/devel/setup.bash --extend
+source /home/$USER/pandora_ws/devel/setup.bash --extend
 
 #source /home/$USER/scripts/launch_config_params.sh
 #sleep 10
