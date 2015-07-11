f52f241061a53dca9a1ad36220c90114de015143
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-07-11 19:45:01 +0300

    Minor changes

--- ledstrip_controller/scripts/ledstrip_controller/ledstrip_controller.py
+++ ledstrip_controller/scripts/ledstrip_controller/ledstrip_controller.py
@@ -41,96 +41,194 @@ import rospy
 import pigpio
 import json
 from state_manager_msgs.msg import RobotModeMsg
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Float64
+import roslib
+roslib.load_manifest("ledstrip_controller")
 
+# Dynamic Reconfigure Server
+from dynamic_reconfigure.server import Server as DynRecServer
+
+#from ledstrip_controller import ledstrip_controllerConfig as Config
 
 class LedstripController():
 
-  ##
-  #  Default constructor
-  #
-  def __init__(self):
+    ##
+    #  Default constructor
+    #
+    def __init__(self):
+        self.led_state_ = False
+        self.brightness_ = 1.0
+        self.pwm_duty_ = 255
 
-    self.led_state_ = False
-    self.gpio_handler_ = pigpio.pi()
+        self.pwm_range_ = [0, 255]
+        self.bright_range_ = [0, 1]
+        self.gpio_handler_ = pigpio.pi()
 
-    # ----< Load parameters from parameter server >---- #
-    self.ledstrip_gpio_ = rospy.get_param( "~led_gpio_pin", 26)
+        # ----< Load parameters from parameter server >---- #
+        self.ledstrip_gpio_ = rospy.get_param( "~led_gpio_pin", 26)
 
-    self.topic_cmd_ = rospy.get_param( "~subscribed_topics/ledstrip_cmd", \
-        "/kinect/ledstrip/state_cmd")
+        self.topic_cmd_ = rospy.get_param( "~subscribed_topics/ledstrip_cmd", \
+                "/kinect/ledstrip/state_cmd")
 
-    self.topic_state_client_ = rospy.get_param( \
-        "~subscribed_topics/state_client", "/robot/state/clients")
+        self.topic_bright_ = rospy.get_param( "~subscribed_topics/brightness", \
+                "/kinect/ledstrip/brightness")
 
-    self.topic_state_ = rospy.get_param( \
-        "~published_topics/ledstrip_state", "/ledstrip/state")
+        self.topic_state_client_ = rospy.get_param( \
+                "~subscribed_topics/state_client", "/robot/state/clients")
 
-    self.pub_rate_ = rospy.get_param("~pub_rate", 1)
-    # ------------------------------------------------- #
+        self.topic_state_ = rospy.get_param( \
+                "~published_topics/ledstrip_state", "/kinect/ledstrip/state")
 
-    # ---------------------< Subscribers >---------------------- #
-    rospy.Subscriber(self.topic_cmd_, Bool, self.command_callback)
-    rospy.Subscriber(self.topic_state_client_, RobotModeMsg, \
-            self.state_change_callback)
-    # ---------------------------------------------------------- #
+        self.pub_rate_ = rospy.get_param("~pub_rate", 1)
+        # ------------------------------------------------- #
 
-    # ----------------------< Publishers >---------------------- #
-    self.state_pub_ = rospy.Publisher(self.topic_state_, Bool, queue_size=1)
-    # ---------------------------------------------------------- #
+        # ---------------------< Subscribers >---------------------- #
+        rospy.Subscriber(self.topic_cmd_, Bool, self.__command_callback)
 
-    # -----------< Initiate gpio to control ledstrip > --------- #
-    self.gpio_handler_.set_mode(self.ledstrip_gpio_, 1)
-    self.gpio_handler_.write(self.ledstrip_gpio_, 0)
-    # ---------------------------------------------------------- #
+        rospy.Subscriber(self.topic_state_client_, RobotModeMsg, \
+                self.__state_change_callback)
 
-    # Start Publishing timer 
-    rospy.Timer(rospy.Duration(float(1/self.pub_rate_)), \
-            self.publish_led_state_callback, oneshot=False)
+        rospy.Subscriber(self.topic_bright_, Float64, 
+                self.__brightness_callback)
+        # ---------------------------------------------------------- #
 
+        # ----------------------< Publishers >---------------------- #
+        self.state_pub_ = rospy.Publisher(self.topic_state_, Float64, 
+                queue_size=1)
+        # ---------------------------------------------------------- #
 
-  ##
-  #  
-  def state_change_callback(self, data):
-    if data.mode == data.MODE_IDENTIFICATION or \
-            data.mode == data.MODE_SENSOR_HOLD:
-      self.led_control(1)
-    else:
-      self.led_control(0)
+        # -----------< Initiate gpio to control ledstrip > --------- #
+        self.gpio_handler_.set_mode(self.ledstrip_gpio_, 1)
+        #self.gpio_handler_.write(self.ledstrip_gpio_, 0)
+        self.__led_control(0)
+        # ---------------------------------------------------------- #
 
-
-  def command_callback(self, msg):
-      self.led_state = msg.data
-      rospy.loginfo("Turning Led : %s" % msg.data)
-      self.led_control(msg.data)
-
-
-  def publish_led_state_callback(self, event):
-    msg = Bool(self.led_state_)
-    self.state_pub_.publish(msg)
+        #self.dyn_rec_server_ = DynRecServer(Config, self.reconfigure)
+        
+        # Start Publishing timer 
+        rospy.Timer(rospy.Duration(float(1/self.pub_rate_)), \
+                self.__publish_led_state_callback, oneshot=False)
+    # ======================================================================= #
 
 
-  def led_control(self, value):
-      try:
-        self.gpio_handler_.write(self.ledstrip_gpio_, value)
-      except:
-        e = sys.exc_info()[0]
-        rospy.logfatal(\
-          "[Ledstrip-Controller]: Failed to set Ledstrip State")
-        rospy.logfatal(e)
-        return False
-      finally:
-          self.led_state_ = value
-          rospy.loginfo("[Ledstrip-Controller]: Setting ledstrip State ---> %s"\
-            % value)
-          return True
+    ##
+    #  Callback for Dynamic Reconfigure Server
+    #
+    #def reconfigure(self, config, level):
+        #self.brightness_ = config['brightness']
+        #return config
+
+
+    ##
+    #  Default destructor
+    ##
+    def __del__(self):
+        self.__led_control(0)
+        del self.gpio_handler_
+    # ======================================================================= #
+
+
+    ##
+    #  TODO
+    ##
+    def __state_change_callback(self, data):
+        if data.mode == data.MODE_IDENTIFICATION or \
+                data.mode == data.MODE_SENSOR_HOLD:
+            self.__led_control(1)
+        else:
+            self.__led_control(0)
+    # ======================================================================= #
+
+
+    ##
+    #  TODO
+    ##
+    def __brightness_callback(self, msg):
+        bright_val = msg.data
+        if bright_val > 1.0:
+            bright_val = 1.0
+
+        rospy.loginfo("[Ledstrip-Controller]: Setting brightness ---> %s"\
+                % bright_val)
+        self.__set_brightness(bright_val)
+    # ======================================================================= #
+
+
+    ##
+    #  TODO
+    ##
+    def __command_callback(self, msg):
+        self.led_state = msg.data
+        #rospy.loginfo("Turning Led : %s" % msg.data)
+        self.__led_control(msg.data)
+    # ======================================================================= #
+
+
+    ##
+    #  TODO
+    ##
+    def __publish_led_state_callback(self, event):
+        msg = Float64(self.brightness_)
+        self.state_pub_.publish(msg)
+    # ======================================================================= #
+
+
+    ##
+    #  TODO
+    ##
+    def __set_brightness(self, bright_val):         
+        self.pwm_duty_ = self.linear_conversion(self.bright_range_,
+                self.pwm_range_, bright_val)
+        if self.__led_control(1):
+            self.brightness_ = bright_val
+    # ======================================================================= #
+
+
+    ##
+    #  TODO
+    ##
+    def __led_control(self, value):
+        try:
+            if value == 1:
+                self.gpio_handler_.set_PWM_dutycycle(self.ledstrip_gpio_, 
+                        self.pwm_duty_)
+            else:
+                self.gpio_handler_.set_PWM_dutycycle(self.ledstrip_gpio_, 0)
+        except:
+            e = sys.exc_info()[0]
+            rospy.logfatal(\
+                    "[Ledstrip-Controller]: Failed to set Ledstrip State")
+            rospy.logfatal(e)
+            return False
+        finally:
+            self.led_state_ = value
+            rospy.loginfo("[Ledstrip-Controller]: Setting ledstrip State ---> %s"\
+                    % value)
+            return True
+    # ======================================================================= #
+
+
+    ## 
+    #  Linear conversion function
+    #  @param old_range [min,max]
+    #  @param new_range [min,max]
+    #  @param value Value to perform linear conversion
+    ##
+    def linear_conversion(self, old_range, new_range, value):
+        #a = old_range[1] - old_range[0]
+        #b = new_range[1] - new_range[0]
+        new_value = 1.0 *(value - old_range[0]) / (old_range[1] - old_range[0])
+        new_value  = new_value * (new_range[1] - new_range[0]);
+        new_value += new_range[0]
+        return new_value
+    # ======================================================================= #
 
 
 
 def main():
-  rospy.init_node("ledstrip_controller")
-  ledstrip_ctrl = LedstripController()
-  rospy.spin()
+    rospy.init_node("ledstrip_controller")
+    ledstrip_ctrl = LedstripController()
+    rospy.spin()
 
 
 if __name__ == "__main__":
--- ledstrip_controller/setup.py
+++ ledstrip_controller/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages = ['ledstrip_switch'],
+    packages = ['ledstrip_controller'],
     package_dir = {'': 'scripts'}
 )
 
